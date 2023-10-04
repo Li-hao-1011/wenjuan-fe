378be26e7f41e5b2e484ed12a76fb3f0d0c73d3e
lihao <lihao.coder.foxmail.com>
2023-10-04 10:39:09 +0800

    修改统计页

--- src/pages/question/stat/index.tsx
+++ src/pages/question/stat/index.tsx
@@ -2,11 +2,11 @@ import { FC } from 'react'
 import useLoadQuestionData from '../../../hooks/useLoadQuestionData'
 
 const Stat: FC = () => {
-  const { loading, questionData } = useLoadQuestionData()
+  const { loading } = useLoadQuestionData()
   return (
     <>
       <h1>Stat</h1>
-      {loading ? <p>loading</p> : <p>{JSON.stringify(questionData)}</p>}
+      {loading ? <p>loading</p> : <p></p>}
     </>
   )
 }

abf37ad408eebd63e1f4fac41cf2db97e2ef5804
lihao <lihao.coder.foxmail.com>
2023-10-04 10:53:25 +0800

    question页面样式等

--- src/pages/question/edit/index.tsx
+++ src/pages/question/edit/index.tsx
@@ -1,14 +1,41 @@
 import { FC } from 'react'
+import { useDispatch } from 'react-redux'
+import { changeSelectedId } from '../../../store/questionReducer'
 import useLoadQuestionData from '../../../hooks/useLoadQuestionData'
-
+import styles from './index.module.scss'
+import EditCanvas from './EditCanvas'
+import LeftPanel from './LeftPanel'
+import RightPanel from './RightPanel'
+import EditHeader from './EditHeader'
 const Edit: FC = () => {
-  const { loading, questionData } = useLoadQuestionData()
+  const { loading } = useLoadQuestionData()
+  const dispatch = useDispatch()
+  const cancelSelected = () => {
+    dispatch(changeSelectedId(''))
+  }
 
   return (
-    <>
-      <h1>Edit</h1>
-      {loading ? <p>loading</p> : <p>{JSON.stringify(questionData)}</p>}
-    </>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <EditHeader />
+      </header>
+      <main className={styles['content-wrapper']}>
+        <div className={styles.content}>
+          <div className={styles.left}>
+            <LeftPanel />
+          </div>
+          <div className={styles.main} onClick={() => cancelSelected()}>
+            <div className={styles['canvas-wrapper']}>
+              {/* <div style={{ height: '1000px', width: '1000px' }}>画布</div> */}
+              <EditCanvas loading={loading} />
+            </div>
+          </div>
+          <div className={styles.right}>
+            <RightPanel />
+          </div>
+        </div>
+      </main>
+    </div>
   )
 }
 export default Edit

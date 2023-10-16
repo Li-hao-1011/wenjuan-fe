f260acad7b584eefaa2bc3fbdafed379e4beeacc
lihao <lihao.coder.foxmail.com>
2023-10-16 22:01:40 +0800

    解决自动保存问题

--- src/hooks/useGetPageInfo.tsx
+++ src/hooks/useGetPageInfo.tsx
@@ -3,14 +3,16 @@ import { StoreType } from '../store/index'
 import { PageInfoType } from '../store/pageInfoReducer'
 
 export const useGetPageInfo = () => {
-  const {
+  /*   const {
     title,
     desc = '',
     js = '',
     css = '',
     isPublished,
   } = useSelector<StoreType, PageInfoType>((state) => state.pageInfo)
-  return { title, desc, js, css, isPublished }
+  return { title, desc, js, css, isPublished } */
+
+  return useSelector<StoreType, PageInfoType>((state) => state.pageInfo)
 }
 
 export default useGetPageInfo
